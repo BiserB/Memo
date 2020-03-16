9e239dcb4aa4ed094030f27f1a3c3da906cfd7cf
BiserB <butoff@abv.bg>
2020-03-16 22:44:07 +0200

    Change Note model to be able to notify for changes

--- Memo/Memo/HomePage.xaml.cs
+++ Memo/Memo/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Memo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace Memo
         }
         async void OnAddNoteClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new AddNotePage());
+            await Navigation.PushAsync(new AddNotePage() { BindingContext = new Note() });
         }
 
     }
--- Memo/Memo/Models/Note.cs
+++ Memo/Memo/Models/Note.cs
@@ -1,17 +1,65 @@
 ﻿using SQLite;
 using System;
+using System.ComponentModel;
 
 namespace Memo.Models
 {
-    public class Note
+    public class Note : INotifyPropertyChanged
     {
+        private int id;
+        private string text;
+        private DateTime createdOn;
+        private DateTime? updatedOn;
+        
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                this.OnPropertyChanged(nameof(Id));
+            }
+        }
 
-        public string Text { get; set; }
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                this.OnPropertyChanged(nameof(Text));
+            }
+        }
 
-        public DateTime CreatedOn { get; set; }
+        public DateTime CreatedOn
+        {
+            get { return createdOn; }
+            set
+            {
+                createdOn = value;
+                this.OnPropertyChanged(nameof(CreatedOn));
+            }
+        }
 
-        public DateTime? UpdatedOn { get; set; }
+        public DateTime? UpdatedOn
+        {
+            get { return updatedOn; }
+            set
+            {
+                updatedOn = value;
+                this.OnPropertyChanged(nameof(UpdatedOn));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
